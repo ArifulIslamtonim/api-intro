53899493b02311f50da763b86e65a032085e201a
AriulIslamtonim <arifulislamtonim@gmail.com>
2022-02-22 13:53:08 +0600

    fetch js

--- api.js
+++ api.js
@@ -3,3 +3,16 @@ function loadData() {
     .then(response => response.json())
     .then(json => console.log(json));
 }
+
+
+function loadUsers() {
+   fetch('https://jsonplaceholder.typicode.com/posts')
+   .then(responsive => responsive.json())
+   .then(json => console.log(json));
+}
+function loadTodos(){
+    fetch('https://jsonplaceholder.typicode.com/todos')
+    .then(responsive => responsive.json())
+    .then(json => console.log(json))
+
+}
